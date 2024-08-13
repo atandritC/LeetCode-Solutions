431cde64c0e760ed8837642c0ac9f5403b083585
Atandrit Chatterjee <atandrit04chatterjee@gmail.com>
2024-08-13 19:06:15 +0530

    Time: 5 ms (70.87%), Space: 43.7 MB (86.46%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+
+        int rev = 0, temp = x;
+
+        while (temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
+        }
+
+        return (rev == x ? true : false);
+    }
+}
